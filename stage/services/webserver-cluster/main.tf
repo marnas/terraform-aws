b080a6b084c2ee22237ccb2b933256dfb775c742
Marco Santonastaso <m.santonastaso@protonmail.com>
2019-06-04 11:28:41 +0100

    region updated & image_id variable implemented

--- stage/services/webserver-cluster/main.tf
+++ stage/services/webserver-cluster/main.tf
@@ -1,18 +1,19 @@
 # setting AWS provider
 # region London eu-west-2
 provider "aws" {
-  region = "us-east-1"
+  region = "eu-west-2"
 }
 
 module "webserver_cluster" {
   source = "../../../../modules/services/webserver-cluster/"
 
-  cluster_name           = "webservers-stage"
-  db_remote_state_bucket = "terraform-state-marnas"
-  db_remote_state_key    = "stage/data-stores/mysql/terraform.tfstate"
-  instance_type = "t2.micro"
-  min_size      = 2
-  max_size      = 4
+  cluster_name            = "webservers-stage"
+  db_remote_state_bucket  = "terraform-state-marnas"
+  db_remote_state_key     = "stage/data-stores/mysql/terraform.tfstate"
+  image_id                = "ami-7ad7c21e"
+  instance_type           = "t2.micro"
+  min_size                = 2
+  max_size                = 4
 
 }
 
